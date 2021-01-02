5c92526311344996e2c8bedf923c6613132fc2c1
RieIijima <yusei.onigiri@gmail.com>
2021-01-02 17:59:46 +0900

    ArgumentError

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   end
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
+  resources :favorites, only: [:create, :destroy]
 end
