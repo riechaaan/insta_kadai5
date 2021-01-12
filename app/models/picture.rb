cb7e8e007abc3f66a93c3d57a8df75ea538c0013
　riechaaan <yusei.onigiri@gmail.com>
2021-01-12 14:23:38 +0900

    picture投稿バリデーション

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,7 @@
 class Picture < ApplicationRecord
   mount_uploader :image, ImageUploader
+  validates :image, presence: true
+  validates :content, presence: true
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
